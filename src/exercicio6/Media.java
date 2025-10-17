2417f1e6fbb269ae52e94f92a52f309db0cd5170
guiper43 <87154378+guiper43@users.noreply.github.com>
2025-10-17 16:05:03 -0300

    exercicio 6 finalizado

--- src/exercicio6/Media.java
+++ src/exercicio6/Media.java
@@ -3,17 +3,25 @@ package exercicio6;
 import java.util.Scanner;
 
 public class Media {
-    public void calcularMedia(){
+    public void calcularMedia() {
         Scanner teclado = new Scanner(System.in);
-        System.out.println("digite a primeira nota a ser calculada.");
+        System.out.println("Digite a primeira nota a ser calculada.");
         Double n1 = teclado.nextDouble();
-        System.out.println("digite a segunda nota a ser calculada.");
+        System.out.println("Digite a segunda nota a ser calculada.");
         Double n2 = teclado.nextDouble();
-        System.out.println("digite a tercera nota a ser calculada.");
+        System.out.println("Digite a terceira nota a ser calculada.");
         Double n3 = teclado.nextDouble();
-        System.out.println("digite a quarta nota a ser calculada.");
+        System.out.println("Digite a quarta nota a ser calculada.");
         Double n4 = teclado.nextDouble();
-        Double media = (n1 + n2 + n3 + n4) /4;
-        System.out.println("o total da sua média calculada é :" + media);
+        Double media = (n1 + n2 + n3 + n4) / 4;
+if (media >= 7.0){
+    System.out.println("Você foi aprovado, parabêns sua média foi:"+ media);
+} else if ((media >= 5.0) && (media< 7.0)) {
+    System.out.println("Você ficou em recuperação. Sua média atingida foi: "+ media + "você precisa atingir no mínimo 7,0 para ser aprovado.");
+} else {
+    System.out.println("Infelismente fvocê foi reprovado. Sua média atingida foi: " + media);
+}
+teclado.close();
     }
+
 }
